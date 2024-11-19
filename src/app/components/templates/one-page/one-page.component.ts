b30fb2e6e8745870dd3b9f642b89e1544049c496
ElephantC-PL <elephantc@kubaadamczyk.pl>
2024-11-19 11:08:32 +0100

    testowy signalStore

--- src/app/components/templates/one-page/one-page.component.ts
+++ src/app/components/templates/one-page/one-page.component.ts
@@ -2,13 +2,15 @@ import { Component, inject, Input } from '@angular/core';
 import { Page } from '../../../models/page.model';
 import { HeaderComponent } from '../../organisms/header/header.component';
 import { SectionComponent } from '../../organisms/section/section.component';
+import { TestComponent } from '../../test/test.component';
 
 @Component({
   selector: 'app-one-page',
   standalone: true,
   imports: [
     HeaderComponent,
-    SectionComponent
+    SectionComponent,
+    TestComponent
   ],
   templateUrl: './one-page.component.html',
   styleUrl: './one-page.component.scss'
